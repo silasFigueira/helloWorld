e537f48f6530b94d0ec4490058d50015a167f877
Silas Pacheco  Figueira <spfigueira@unicap.br>
2017-07-23 18:02:21 -0300

    arquivo

---
+++ novo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/stylesheet.css" type="text/css">
+        
+        <title></title>
+    </head>
+    <body>
+        <h1>Silas</h1>
+    </body>
+</html>
