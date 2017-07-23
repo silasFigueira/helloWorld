73b7cba062812e11621c1f7d297cda98a6fb0f2c
Silas Pacheco  Figueira <spfigueira@unicap.br>
2017-07-22 22:43:37 -0300

    meu segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< refs/remotes/origin/modifica
 <!DOCTYPE html>
 <html>
     <head>
@@ -17,3 +18,36 @@
         
     </body>
 </html>
+=======
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World!";
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Número</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
+>>>>>>> meu segundo commit
