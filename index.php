ad4e112a232f2401f89fa4abecdba0ea21e207f1
Silas Pacheco  Figueira <spfigueira@unicap.br>
2017-07-23 09:27:46 -0300

    Apagra indesejavel
    apagou o texto indesejável

--- index.php
+++ index.php
@@ -28,10 +28,11 @@
         </table>
         <div>
             <h1>Sou outra alteração!</h1>
-<<<<<<< HEAD
+<<
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor fermentum mauris ac vestibulum. Nulla finibus justo varius sapien auctor, nec rutrum neque sagittis. Sed accumsan lectus vitae diam sodales vestibulum. Proin vitae purus scelerisque, fermentum eros id, ornare turpis. Maecenas blandit, mauris a vestibulum egestas, augue leo feugiat eros, a commodo nisi justo eu sem. Suspendisse tempus metus at mi gravida, eu cursus dolor cursus. Cras in leo imperdiet, tristique ipsum vel, vehicula velit. Donec suscipit diam at tempor pulvinar. Donec viverra, neque id euismod tincidunt, justo mauris vehicula massa, id feugiat nisl orci et lorem. Sed venenatis blandit mauris, quis congue nulla molestie quis. Nam ut eros non felis eleifend semper a sit amet eros.</p>
-=======
->>>>>>> helloworld
+==
         </div>
+        
+        <p>Acabo de apagar um texto indesejável</p>
     </body>
 </html>
