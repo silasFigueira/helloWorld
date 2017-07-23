01cb2b1fd602b6a062593a91629578ba2b81511a
seu nome aqui <spfigueira@uol.com.br>
2017-07-23 14:40:39 -0300

    commit 7

--- index.php
+++ index.php
@@ -5,36 +5,15 @@
         <title></title>
     </head>
     <body>
-        <?php
-        echo "Hello World!";
-        ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Número</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Silas Pacheco Figueira</td>
-                    <td>81-99903-1369</td>
-                </tr>
-                <tr>
-                    <td>Magda Figueira</td>
-                    <td>81-997044472</td>
-                </tr>
-            </tbody>
-        </table>
-        <div>
             <h1>Sou outra alteração!</h1>
+        
+<h1>Mais uma lorem ispum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor fermentum mauris ac vestibulum. Nulla finibus justo varius sapien auctor, nec rutrum neque sagittis. Sed accumsan lectus vitae diam sodales vestibulum. Proin vitae purus scelerisque, fermentum eros id, ornare turpis. Maecenas blandit, mauris a vestibulum egestas, augue leo feugiat eros, a commodo nisi justo eu sem. Suspendisse tempus metus at mi gravida, eu cursus dolor cursus. Cras in leo imperdiet, tristique ipsum vel, vehicula velit. Donec suscipit diam at tempor pulvinar. Donec viverra, neque id euismod tincidunt, justo mauris vehicula massa, id feugiat nisl orci et lorem. Sed venenatis blandit mauris, quis congue nulla molestie quis. Nam ut eros non felis eleifend semper a sit amet eros.</p>
+                <div>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor fermentum mauris ac vestibulum. Nulla finibus justo varius sapien auctor, nec rutrum neque sagittis. Sed accumsan lectus vitae diam sodales vestibulum. Proin vitae purus scelerisque, fermentum eros id, ornare turpis. Maecenas blandit, mauris a vestibulum egestas, augue leo feugiat eros, a commodo nisi justo eu sem. Suspendisse tempus metus at mi gravida, eu cursus dolor cursus. Cras in leo imperdiet, tristique ipsum vel, vehicula velit. Donec suscipit diam at tempor pulvinar. Donec viverra, neque id euismod tincidunt, justo mauris vehicula massa, id feugiat nisl orci et lorem. Sed venenatis blandit mauris, quis congue nulla molestie quis. Nam ut eros non felis eleifend semper a sit amet eros.</p>
 
         </div>
         
-        <p>Acabo de apagar um texto indesejável</p>
-        <h3>DIFERENCIEI!!!</h3>
-        <h2>DIFERENCIEI!!!</h2>
     </body>
 </html>
