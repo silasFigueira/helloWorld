6d622a23f08c62473aacde5c80f8b2ea5ff2d01b
Silas Pacheco Figueira <spfigueira@unicap.br>
2017-07-24 09:01:44 -0300

    corrige html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< refs/remotes/origin/modifica
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,26 +15,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor fermentum mauris ac vestibulum. Nulla finibus justo varius sapien auctor, nec rutrum neque sagittis. Sed accumsan lectus vitae diam sodales vestibulum. Proin vitae purus scelerisque, fermentum eros id, ornare turpis. Maecenas blandit, mauris a vestibulum egestas, augue leo feugiat eros, a commodo nisi justo eu sem. Suspendisse tempus metus at mi gravida, eu cursus dolor cursus. Cras in leo imperdiet, tristique ipsum vel, vehicula velit. Donec suscipit diam at tempor pulvinar. Donec viverra, neque id euismod tincidunt, justo mauris vehicula massa, id feugiat nisl orci et lorem. Sed venenatis blandit mauris, quis congue nulla molestie quis. Nam ut eros non felis eleifend semper a sit amet eros.</p>
 
         </div>
-        
-    </body>
-</html>
-=======
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "Hello World!";
-        ?>
-        <table>
+    <table>
             <thead>
                 <tr>
                     <th>Nome</th>
@@ -50,4 +31,4 @@ and open the template in the editor.
         </table>
     </body>
 </html>
->>>>>>> meu segundo commit
+
